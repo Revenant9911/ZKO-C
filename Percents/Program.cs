7d37078506df84fb18f63cf4380dc839e1ad0000
Dima Rogovskiy <dima_rogovskii@mail.ru>
2020-11-20 21:02:54 +0300

    Rename project

---
+++ Percents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DZ___ZKO
+{
+    class Program
+    {
+        public static double Calculate(string input)
+        {
+            string[] words = input.Split();
+            double inputSum = double.Parse(words[0]);
+            double percent = double.Parse(words[1]);
+            double month = double.Parse(words[2]);
+
+            double result = inputSum * Math.Pow(1.00 + (percent / 12 / 100), month);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+           string input =  Console.ReadLine();
+           Console.WriteLine(Program.Calculate(input));
+        }
+    }
+}
